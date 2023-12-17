e765b732f2c8057ed5a2af0828700ff99e13152d
Gabriel Luciu <gabrielluciu2013@gmail.com>
2023-12-17 21:47:07 +0200

    added sounds

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private bool isPlaying = false;
+    private float delayBetweenSounds = 5f;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnTrackingFound()
+    {
+        if (!isPlaying)
+        {
+            isPlaying = true;
+            InvokeRepeating("PlaySoundWithDelay", 0f, delayBetweenSounds);
+        }
+    }
+
+    public void OnTrackingLost()
+    {
+        if (isPlaying)
+        {
+            isPlaying = false;
+            CancelInvoke("PlaySoundWithDelay");
+            audioSource.Stop();
+        }
+    }
+
+    void PlaySoundWithDelay()
+    {
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+}
